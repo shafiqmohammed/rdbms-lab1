37dc749815fb18d75089ba5b3cfb2fc6e01a72c4
shafiqmohammed <shafiqmohamed2008@gmail.com>
2026-07-17 12:08:02 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -4,3 +4,12 @@ CREATE TABLE Course (
     Credits INT,
     DepartmentID INT
 );
+
+INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID)
+VALUES
+    (101, 'Programming', 4, 1),
+    (102, 'Web Development', 3, 1),
+    (103, 'Database Management systems', 4, 2);
+DESCRIBE Course;
+SELECT * FROM Course;
+
